838e96cb184f1cab7a89a28ecb93666003d6c4c8
henzoparahua <hnz.2894henzop19@gmail.com>
2024-06-09 01:27:34 -0300

    commit

---
+++ dx11zSTD/systemclass.cpp
@@ -0,0 +1,18 @@
+#include "systemclass.h"
+
+SystemClass::SystemClass()
+{
+	m_Input = 0;
+	m_Application = 0;
+}
+
+SystemClass::SystemClass(const SystemClass& other)
+{
+
+}
+
+SystemClass::~SystemClass()
+{
+
+}
+
