8c32bd4ece3b673e6895de4635d5d9b20078900a
henzoparahua <hnz.2894henzop19@gmail.com>
2024-06-19 01:50:06 -0300

    commit

---
+++ dx11zSTD/applicationclass.cpp
@@ -0,0 +1,36 @@
+#include "applicationclass.h"
+
+ApplicationClass::ApplicationClass()
+{
+
+}
+
+ApplicationClass::ApplicationClass(const ApplicationClass& other)
+{
+
+}
+
+ApplicationClass::~ApplicationClass()
+{
+
+}
+
+bool ApplicationClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+	return true;
+}
+
+void ApplicationClass::Shutdown()
+{
+	return;
+}
+
+bool ApplicationClass::Frame()
+{
+	return true;
+}
+
+bool ApplicationClass::Render()
+{
+	return true;
+}
