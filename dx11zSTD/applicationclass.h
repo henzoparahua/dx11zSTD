050282e6a9e9344ae3164e6ba740363e1cbd9c3a
henzoparahua <hnz.2894henzop19@gmail.com>
2024-06-19 01:45:31 -0300

    change

---
+++ dx11zSTD/applicationclass.h
@@ -0,0 +1,26 @@
+#ifndef _APPLICATIONCLASS_H_
+#define _APPLICATIONCLASS_H_
+
+#include <Windows.h>
+
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.3f;
+
+class ApplicationClass
+{
+public:
+	ApplicationClass();
+	ApplicationClass(const ApplicationClass&);
+	~ApplicationClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+};
+
+#endif
