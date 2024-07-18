8a3c68b972eef2d7f57eef5fbdfc3001780f4cd0
henzoparahua <hnz.2894henzop19@gmail.com>
2024-07-18 00:42:21 -0300

    d3d11 complete

--- dx11zSTD/d3dclass.h
+++ dx11zSTD/d3dclass.h
@@ -1,11 +1,11 @@
 #ifndef _D3DCLASS_H_
 #define _D3DCLASS_H_
 
-#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
-#include <d3d12.h>
+#include <d3d11.h>
 #include <DirectXMath.h>
 using namespace DirectX;
 
@@ -21,4 +21,35 @@ public:
 
 	void BeginScene(float, float, float, float);
 	void EndScene();
+
+	ID3D11Device* GetDevice();
+	ID3D11DeviceContext* GetDeviceContext();
+
+	void GetProjectionMatrix(XMMATRIX&);
+	void GetWorldMatrix(XMMATRIX&);
+	void GetOrthoMatrix(XMMATRIX&);
+
+	void GetVideoCardInfo(char*, int&);
+
+	void SetBackBufferRenderTarget();
+	void ResetViewport();
+
+private:
+	bool m_vsync_enabled;
+	int m_videoCardMemory;
+	char m_videoCardDescription[128];
+	IDXGISwapChain* m_swapChain;
+	ID3D11Device* m_device;
+	ID3D11DeviceContext* m_deviceContext;
+	ID3D11RenderTargetView* m_renderTargetView;
+	ID3D11Texture2D* m_depthStencilBuffer;
+	ID3D11DepthStencilState* m_depthStencilState;
+	ID3D11DepthStencilView* m_depthStencilView;
+	ID3D11RasterizerState* m_rasterState;
+	XMMATRIX m_projectionMatrix;
+	XMMATRIX m_worldMatrix;
+	XMMATRIX m_orthoMatrix;
+	D3D11_VIEWPORT m_viewport;
 };
+
+#endif
